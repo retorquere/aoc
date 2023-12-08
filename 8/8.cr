47a59fe687ab2ef13f444a379e1a2e939fd66a9f
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-12-08 15:25:00 +0100

    8a

--- 8/8.cr
+++ 8/8.cr
@@ -1,30 +1,41 @@
 #!/usr/bin/env crystal
 
-#struct Node
-#  @@map = Hash(String, Tuple(String, String)).new
-#
-#  property name
-#  property steps
-#
-#  def initialize(@name : String, left : String, right : String)
-#    @steps = -1
-#    @turn = [ left, right ]
-#  end
-#
-#  def step(turn : Int32)
-#    
-#  end
-#end
-
-template = [] of String
+template = ""
 map = Hash(String, Hash(String, String)).new
+
+struct Node
+  @@map = Hash(String, Tuple(String, String)).new
+
+  @paths : Array(String)
+
+  property name
+
+  def initialize(@name : String, left : String, right : String)
+    @paths = [] of String
+    @turn = { "L" => left, "R" => right }
+  end
+
+  def walk(route : String) : String
+    return "" if @name == "ZZZ"
+
+    route += template if route.empty?
+
+    @paths.each{|p|
+      return p if route + (template * p.size).start_with?(p)
+    }
+
+    @paths << route[0] + @@map[@turn[route[0..0]]].walk(route[1..])
+    return @paths[-1]
+  end
+end
+
 File.each_line("input.txt") {|line|
   case line
     when /^([A-Z]+) = [(]([A-Z]+), ([A-Z]+)[)]/
       map[$1] = { "L" => $2, "R" => $3 }
 
     when /^([LR]+)$/
-      template = line.split("")
+      template = line
 
     when ""
 
@@ -35,10 +46,12 @@ File.each_line("input.txt") {|line|
 
 state = "AAA"
 steps = 0
-route = [] of String
+route = ""
 while state != "ZZZ"
   route += template if route.empty?
-  state = map[state][route.shift]
+  puts map[state]
+  state = map[state][route[0..0]]
+  route = route[1..]
   steps += 1
 end
   
