787c2e1bfa351f45e877c404505e7ce3755627d5
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-12-12 15:16:47 +0100

    faster

--- 12/12.cr
+++ 12/12.cr
@@ -1,23 +1,26 @@
 #!/usr/bin/env crystal
 
 sum = 0
-File.new("sample.txt").each_line{|line|
+File.new("input.txt").each_line{|line|
   map, counts = line.split
 
-  dots = "([.?]+?)"
-  re = Regex.new("^([.?]*?)" + counts.split(",").map{|d| "([#?]{#{d}})#{dots}" }.join("").chomp(dots) + "([.?]*)$")
-  total = counts.split(",").map{|c| c.to_i}.sum
-
-  todo = [ map ]
-  while i = todo.index{|m| m.includes?("?") }
-    t = todo.delete_at(i)
+  dots = "[.?]+?"
+  maybedots = "[.?]*?"
+  re = Regex.new "^" + maybedots + counts.split(",").map{|d| "[#?]{#{d}}#{dots}" }.join("").rstrip(dots) + maybedots + "$"
+  
+  candidates = [ map ]
+  while i = candidates.index{|m| m.includes?("?") }
+    t = candidates.delete_at(i)
     [".", "#"].each{|c|
       c = t.sub("?", c)
-      todo << c if c.gsub(".", "").size >= total
+      candidates << c if re.match(c)
     }
   end
 
-  sum += todo.select{|m| re.match(m)}.size
+  puts line
+  puts candidates
+
+  sum += candidates.size
 }
 puts sum
 
