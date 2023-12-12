7ff8a77c0039d4bf5a4f06816262ce6cabe39f3d
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-12-12 21:14:03 +0100

    wow

--- 12/12.cr
+++ 12/12.cr
@@ -1,26 +1,44 @@
 #!/usr/bin/env crystal
 
-sum = 0
-File.new("input.txt").each_line{|line|
-  map, counts = line.split
+Cache = Hash(String, Int64).new
+def fits(unmatched : String, sizes : Array(Int64))
+  key = "#{unmatched} " + sizes.map{|n| n.to_s}.join(" ")
+  Cache[key] = uncached_fits(unmatched, sizes) if !Cache.has_key?(key)
+  return Cache[key]
+end
 
-  dots = "[.?]+?"
-  maybedots = "[.?]*?"
-  re = Regex.new "^" + maybedots + counts.split(",").map{|d| "[#?]{#{d}}#{dots}" }.join("").rstrip(dots) + maybedots + "$"
-  
-  candidates = [ map ]
-  while i = candidates.index{|m| m.includes?("?") }
-    t = candidates.delete_at(i)
-    [".", "#"].each{|c|
-      c = t.sub("?", c)
-      candidates << c if re.match(c)
-    }
+def uncached_fits(unmatched : String, sizes : Array(Int64)) : Int64
+  unmatched = unmatched.lstrip(".")
+
+  return sizes.empty? ? 1_i64 : 0_i64 if unmatched == ""
+
+  # valid junk at the end
+  return unmatched.includes?("#") ? 0_i64 : 1_i64 if sizes.empty?
+
+  if unmatched[0] == '#' # unmatched starts with '#' so remove the first spring
+    if unmatched.size < sizes[0] || unmatched[0...(sizes[0])].includes?(".")
+      # too small || first stretch cannot be a spring
+      return 0_i64
+    elsif unmatched.size == sizes[0]
+      return sizes.size == 1 ? 1_i64 : 0_i64 # last spring
+    elsif unmatched[sizes[0]] == '#'
+      return 0_i64 # spring too long
+    else
+      return fits(unmatched[(sizes[0] + 1)..], sizes[1..]) # on to the next spring
+    end
   end
 
-  puts line
-  puts candidates
+  # try with leading '?' changed to '#', or omit the leading '.'
+  return fits("#" + unmatched[1..], sizes) + fits(unmatched[1..], sizes)
+end
 
-  sum += candidates.size
-}
-puts sum
-
+part1 = 0_i64
+part2 = 0_i64
+File.new("sample.txt").each_line do |line|
+  map, sizes = line.split
+  sizes = sizes.split(",").map{|n| n.to_i64 }
+  part1 += fits(map, sizes)
+  part2 += fits(([map] * 5).join('?'), sizes * 5)
+end
+puts part1
+puts part2
