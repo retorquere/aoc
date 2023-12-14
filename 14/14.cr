fed4b3ff6b797db6c6bd79410e44bba6322b554a
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-12-14 16:00:56 +0100

    14a

--- 14/14.cr
+++ 14/14.cr
@@ -1,44 +1,32 @@
 #!/usr/bin/env crystal
 
-alias Row = Hash(Symbol, UInt128)
-
 class CPlatform
   @width = 0
-  @rows : Array(Row)
+  @round : Array(UInt128)
+  @cube : Array(UInt128)
 
   property width
-  property rows
+  property round
+  property cube
 
   def initialize
-    @rows = File.read("input.txt").strip.split("\n").map{|row|
+    @round = [] of UInt128
+    @cube = [] of UInt128
+    File.read("sample.txt").strip.split("\n").map{|row|
       @width = row.size
-      {
-        :round => row.gsub(".", "0").gsub("#", "0").gsub("O", "1").to_u128(2),
-        :cube => row.gsub(".", "0").gsub("#", "1").gsub("O", "0").to_u128(2)
-      }
+      @round << row.gsub(".", "0").gsub("#", "0").gsub("O", "1").to_u128(2)
+      @cube << row.gsub(".", "0").gsub("#", "1").gsub("O", "0").to_u128(2)
     }
   end
 
   def n
-    (1 ... @rows.size).each do |times|
-      (1 ... @rows.size).each do |row|
-        up = @rows[row - 1]
-        capacity = ~(up[:round] | up[:cube])
-        move = capacity & @rows[row][:round]
-        @rows[row - 1][:round] += move
-        @rows[row][:round] -= move
-      end
-    end
-  end
-
-  def s
-    (1 ... @rows.size).each do |times|
-      (@rows.size - 1).downto(1) do
-        down = @rows[row + 1]
-        capacity = ~(down[:round] | down[:cube])
-        move = capacity & @rows[row][:round]
-        @rows[row + 1][:round] += move
-        @rows[row][:round] -= move
+    (1 ... @round.size).each do |times|
+      (1 ... @round.size).each do |row|
+        up = row - 1
+        capacity = ~(@round[up] | @cube[up])
+        move = capacity & @round[row]
+        @round[up] += move
+        @round[row] -= move
       end
     end
   end
@@ -48,14 +36,15 @@ class CPlatform
   end
 
   def weight
-    @rows.each_with_index.map{|row, i|
-      row[:round].to_s(2).count("1") * (@rows.size - i)
+    @round.each_with_index.map{|row, i|
+      row.to_s(2).count("1") * (@round.size - i)
     }.sum
   end
 
   def show
     puts "-" * @width
-    @rows.each do |row|
+    rows = @round.zip(@cube).map{|round, cube| { round: round, cube: cube }}
+    rows.each do |row|
       puts self.chars(row[:round]).zip(self.chars(row[:cube])).map{|rc| {"00" => ".", "10" => "O", "01" => "#"}[rc.join("")] }.join("")
     end
   end
