8dcbdbed767d98abf34bafb57f005ec39369629b
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-12-17 22:40:58 +0100

    counting

--- 17/17.cr
+++ 17/17.cr
@@ -1,45 +1,47 @@
 #!/usr/bin/env crystal
 
+require "benchmark"
+
 R = {0, +1}
 D = {+1, 0}
 L = {0, -1}
 U = {-1, 0}
 
 alias Coord = Tuple(Int32, Int32)
-alias State = NamedTuple(cost: Int32, cell: Coord, moves: Array(Coord))
+alias State = NamedTuple(cost: Int32, cell: Coord, move: Coord, n: Int32)
 
 Grid = Hash(Coord, Int32).new
 
-File.read("sample.txt").chomp.split("\n").each_with_index do |line, row|
+File.read("input.txt").chomp.split("\n").each_with_index do |line, row|
   line.chars.each_with_index do |cost, col|
     Grid[{row, col}] = cost.to_i
   end
 end
 
 def search(max_same)
-  visited = Set(NamedTuple(cell: Coord, moves: Array(Coord))).new
-  open = [{ cost: 0, cell: {0, 0}, moves: [R] }, { cost:0, cell: {0, 0}, moves: [D] }]
+  visited = Set(NamedTuple(cell: Coord, move: Coord, n: Int32)).new
+  open = [{ cost: 0, cell: {0, 0}, move: R, n: 1 }, { cost:0, cell: {0, 0}, move: D, n: 1 }]
   goal = Grid.keys.max
 
   until open.empty?
     cheapest = open.shift
 
-    next unless visited.add?(NamedTuple(cell: Coord, moves: Array(Coord)).from(cheapest.to_h.reject(:cost)))
+    next unless visited.add?({ cell: cheapest[:cell], move: cheapest[:move], n: cheapest[:n] })
 
-    cell = {cheapest[:cell][0] + cheapest[:moves][0][0], cheapest[:cell][1] + cheapest[:moves][0][1]}
+    cell = {cheapest[:cell][0] + cheapest[:move][0], cheapest[:cell][1] + cheapest[:move][1]}
     next unless Grid.has_key?(cell)
 
     cost = cheapest[:cost] + Grid[cell]
-    if cheapest[:moves].size <= max_same
+    if cheapest[:n] <= max_same
       return cost if cell == goal
     end
 
     [R, D, L, U].each do |move|
       # can't reverse
-      next if move.to_a.zip(cheapest[:moves][0].to_a).map{|dir| dir.sum}.uniq == [0]
-      moves = cheapest[:moves][0] == move ? [ move ] + cheapest[:moves] : [ move ]
-      next if moves.size > max_same
-      open << { cost: cost, cell: cell, moves: moves }
+      next if move.to_a.zip(cheapest[:move].to_a).map{|dir| dir.sum}.uniq == [0]
+      n = cheapest[:move] == move ? cheapest[:n] + 1 : 1
+      next if n > max_same
+      open << { cost: cost, cell: cell, move: move, n: n }
     end
     open.sort_by!{|state| state[:cost]}
   end
