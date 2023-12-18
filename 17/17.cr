ff227f051468ff1f5630366d87795e0a90fda1f6
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2023-12-18 05:25:12 +0100

    finally

--- 17/17.cr
+++ 17/17.cr
@@ -2,55 +2,60 @@
 
 require "priority-queue"
 
+R = {0, +1}
+D = {+1, 0}
+L = {0, -1}
+U = {-1, 0}
+
 alias Coord = Tuple(Int32, Int32)
-alias State = Tuple(Int32, Coord, Int32, Int32) # cost, pos, dir, n
+alias Candidate = Tuple(Int32, Coord, Coord, Int32)
+alias State = Tuple(Coord, Coord, Int32)
 
 Grid = Hash(Coord, Int32).new
 
-File.read("sample.txt").chomp.split("\n").each_with_index do |line, row|
+File.read("input.txt").chomp.split("\n").each_with_index do |line, row|
   line.chars.each_with_index do |cost, col|
     Grid[{row, col}] = cost.to_i
   end
 end
 
-Moves = [{1, 0}, {0, 1}, {-1, 0}, {0, -1}]
-
-def solve(min, max)
-  open = Priority::Queue(State).new
-  open.push(0, {0, Grid.keys.min, 0, 0})
-  open.push(0, {0, Grid.keys.min, 1, 0})
-  visited = Set(Tuple(Coord, Int32, Int32)).new
+def search(min, max)
+  visited = Set(State).new
+  open = Priority::Queue(Candidate).new
+  [{ 0, Grid.keys.min, R, 1 }, { 0, Grid.keys.min, D, 1 }].each do |o|
+    open.push(0, o)
+  end
+  goal = Grid.keys.max
 
   until open.empty?
     cost, current, dir, n = open.pop.value
 
-    next unless visited.add?({current, dir, n})
+    next unless visited.add?({ current, dir, n })
 
-    if current == Grid.keys.max
-      if n >= min
-        puts cost
-        return
-      end
-    end
-
-    turn = Moves[dir]
-    neighbour = { current[0] + turn[0], current[1] + turn[1] }
+    neighbour = {current[0] + dir[0], current[1] + dir[1]}
     next unless Grid.has_key?(neighbour)
 
     cost += Grid[neighbour]
-    n += 1
-
-    if n < max
-      open.push(-cost, { cost, neighbour, dir, n })
+    if n >= min && n <= max
+      return cost if neighbour == goal
     end
 
-    if n >= min
-      open.push(-cost, {cost, neighbour, (dir + 1) % 4, 0})
-      open.push(-cost, {cost, neighbour, (dir - 1) % 4, 0})
+    [R, D, L, U].each do |move|
+      # can't reverse
+      next if { dir[0] + move[0], dir[1] + move[1] } == {0, 0}
+
+      # no turn unless min distance covered
+      next if move != dir && n < min
+
+      # don't proceed if crucible gets wobbly
+      move_n = dir == move ? n + 1 : 1
+      next if move_n > max
+
+      open.push(-cost, { cost, neighbour, move, move_n })
     end
   end
   return -1
 end
 
-solve(1, 3)
-solve(4, 10)
+puts search(1, 3)
+puts search(4, 10)
