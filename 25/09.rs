d33a8ec2d9d9d355adfa3e5585dbcff1ba421b79
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2025-12-09 14:17:53 +0100

    09

--- 25/09.rs
+++ 25/09.rs
@@ -1,27 +1,26 @@
 fn main() {
   let input = include_str!("09s.txt");
 
-  let mut tiles: Vec<Vec<usize>> = input
+  let mut tiles: Vec<(usize, usize)> = input
     .trim()
     .split('\n')
     .map(|line| {
-      line.split(',').map(|n| n.trim().parse::<usize>().unwrap()).collect()
+      let dim = line.split(',').map(|n| n.trim().parse::<usize>().unwrap()).collect::<Vec<usize>>();
+      (dim[0], dim[1])
     })
     .collect();
-
+  tiles.sort(); // WTF?!?!?
+  println!("{:?}", tiles);
   let n: usize = tiles.len() as usize;
 
-  let mut rects: Vec<(usize, usize, usize)> = Vec::new();
+  let mut areas: Vec<usize> = Vec::new();
   for i in 0..n {
     for j in (i + 1)..n {
-      let size = &tiles[i].iter()
-        .zip(&tiles[j])
-        .map(|(&c1, &c2)| ((c1 as i64) - (c2 as i64) + 1i64).abs() as usize)
-        .product::<usize>();
-      rects.push((i, j, *size));
+      let (t1, t2) = (&tiles[i], &tiles[j]);
+      areas.push(((t1.0 as i64 - t2.0 as i64 + 1).abs() * (t1.1 as i64 - t2.1 as i64 + 1).abs()) as usize);
     }
   }
-  rects.sort_by_key(|e| e.2);
+  areas.sort();
   // println!("{:?}", rects);
-  println!("1: {}", rects.last().unwrap().2);
+  println!("1: {}", areas.last().unwrap());
 }
